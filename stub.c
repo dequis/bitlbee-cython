ad7a19976a9718ca1f543fc03bd8466aeb395e20
dequis <dx@dxzone.com.ar>
2015-03-25 03:28:53 -0300

    stub: make it slightly more python3 friendly, also use Py_InitializeEx

--- stub.c
+++ stub.c
@@ -2,8 +2,18 @@
 #include "bitlbee_cython.h"
 
 void init_plugin() {
-    Py_Initialize();
+    PyObject* m = NULL;
+
+    /* passing 0 here to skip initialization of signal handlers */
+    Py_InitializeEx(0);
+
+#if PY_MAJOR_VERSION < 3
     initbitlbee_cython();  /* python2-style module initializer name */
+#else
+    m = PyInit_bitlbee_cython();
+#endif
+    Py_XDECREF(m);
+
     init_py_plugin();
 }
 
